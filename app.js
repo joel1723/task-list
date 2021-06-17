4ec8c9a1c3d8e925cf8f2c8a65ae9630c5cea7a0
joel1723 <75437050+joel1723@users.noreply.github.com>
2021-06-17 13:40:19 -0400

    task list

--- app.js
+++ app.js
@@ -4,3 +4,51 @@
     const clearBtn = document.querySelector(".clear-task");
     const filter = document.querySelector("#filter");
     const taskInput= document.querySelector("#task");
+
+
+
+    // Load all event listers
+    loadEventListers();
+
+    // load all event listers
+    function loadEventListers(){
+        // add task event
+        form.addEventListener("submit", addTask);
+    }
+
+    // add task
+    function addTask(e) {
+        if(taskInput.value ===""){
+          alert("add a task ")
+
+          
+        }
+
+        // create li elements
+        const li = document.createElement("li");
+        //add class 
+        li.className= " collection-item";
+        // create text node  and append to li
+        li.appendChild(document.createTextNode(taskInput.value));
+        //create new link element 
+        const link = document.createElement("a")
+        // add class 
+        link.className= "delete-item  secondary-content ";
+        // add icon html 
+        link.innerHTML = '< i class = "fa fa-remove"><i/>';
+        //append the link to the li 
+        li.appendChild(link);
+         // append li to ul 
+         taskList.appendChild(li);
+
+
+
+
+         //clear input 
+         taskInput.value= '';
+        
+
+        e.preventDefault();    
+    }
+
+
