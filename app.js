fa3f68e8f627bd55048753f34c22a6d65b5e85b4
joel1723 <joeljean1723@gmail.com>
2021-06-25 10:22:55 -0400

    delete button

--- app.js
+++ app.js
@@ -1,54 +1,57 @@
-//define ui vars
-    const form = document.querySelector("#task-form");
-    const taskList=document.querySelector(".collection");
-    const clearBtn = document.querySelector(".clear-task");
-    const filter = document.querySelector("#filter");
-    const taskInput= document.querySelector("#task");
+// Define UI Vars
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('.collection');
+const clearBtn = document.querySelector('.clear-tasks');
+const filter = document.querySelector('#filter');
+const taskInput = document.querySelector('#task');
+
+// Load all event listeners
+loadEventListeners();
+
+// Load all event listeners
+function loadEventListeners() {
+  // Add task event
+  form.addEventListener('submit', addTask);
+  //remove task event
+  taskList.addEventListener('click', removeTask)
+}
+
+// Add Task
+function addTask(e) {
+  if(taskInput.value === '') {
+    alert('Add a task');
+  }
+
+  // Create li element
+  const li = document.createElement('li');
+  // Add class
+  li.className = 'collection-item';
+  // Create text node and append to li
+  li.appendChild(document.createTextNode(taskInput.value));
+  // Create new link element
+  const link = document.createElement('a');
+  // Add class
+  link.className = 'delete-item secondary-content';
+  // Add icon html
+  link.innerHTML = '<i class="fa fa-remove"></i>';
+  // Append the link to li
+  li.appendChild(link);
+
+  // Append li to ul
+  taskList.appendChild(li);
+
+  // Clear input
+  taskInput.value = '';
+
+  e.preventDefault();
+}
+function removeTask(e){
+  if(e.target.parentElement.contains('delete-item')){
+    if(confirm('are you sure'))
+    e.target.parentElement.parentElement.remove()
+    
 
 
+  }
 
-    // Load all event listers
-    loadEventListers();
-
-    // load all event listers
-    function loadEventListers(){
-        // add task event
-        form.addEventListener("submit", addTask);
-    }
-
-    // add task
-    function addTask(e) {
-        if(taskInput.value ===""){
-          alert("add a task ")
-
-          
-        }
-
-        // create li elements
-        const li = document.createElement("li");
-        //add class 
-        li.className= " collection-item";
-        // create text node  and append to li
-        li.appendChild(document.createTextNode(taskInput.value));
-        //create new link element 
-        const link = document.createElement("a")
-        // add class 
-        link.className= "delete-item  secondary-content ";
-        // add icon html 
-        link.innerHTML = '< i class = "fa fa-remove"><i/>';
-        //append the link to the li 
-        li.appendChild(link);
-         // append li to ul 
-         taskList.appendChild(li);
-
-
-
-
-         //clear input 
-         taskInput.value= '';
-        
-
-        e.preventDefault();    
-    }
-
-
+}
